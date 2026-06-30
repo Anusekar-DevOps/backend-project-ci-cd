606422480c139c25ed5111b13ae23a67a7d7c111
aswin-sv <aswinfarozzz@gmail.com>
2026-06-30 22:45:26 +0530

    firstcommit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('CI/CD Pipeline Project Running Successfully');
+});
+
+app.get('/health', (req, res) => {
+    res.json({
+        status: 'UP'
+    });
+});
+
+app.listen(5000, () => {
+    console.log('Server started on port 5000');
+});

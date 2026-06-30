4a668a0b058d8ad134f2203d7776c18d248bc13b
aswin-sv <aswinfarozzz@gmail.com>
2026-06-30 23:22:52 +0530

    secondcommit

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('CI/CD Pipeline Project Running Successfully');
+    res.send('Pipeline Project Running Successfully');
 });
 
 app.get('/health', (req, res) => {
